633df6bd8102b307570953d5b4652cb6d76b8e22
Stephan Fowler <stephanfowler@gmail.com>
2015-06-19 17:28:00 +0100

    Persist pg url for local use

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.get('/', function(request, response) {
 });
 
 app.get('/db', function (request, response) {
-  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+  pg.connect(process.env.DATABASE_URL + '?ssl=true', function(err, client, done) {
     client.query('SELECT * FROM test_table', function(err, result) {
       done();
       if (err)
