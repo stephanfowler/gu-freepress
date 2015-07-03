61238d04e6fbe5fbfc8fda7ef3bc211535c6a7a1
Stephan Fowler <stephanfowler@gmail.com>
2015-07-03 16:58:11 +0100

    Load into telegraph etc

--- chrome-extension/inject-iframe.js
+++ chrome-extension/inject-iframe.js
@@ -1,18 +1,37 @@
-var 
+var thisUrl = window.location.href, 
+
     //domain = 'localhost:5000',
     domain = 'quiet-island-1381.herokuapp.com',
-    url = '//' + domain + '/?parentUrl=' + window.location.href,
-    articleEl = document.querySelector('.content__secondary-column .js-mpu-ad-slot'),
-    bannerAdEl = document.querySelector('.top-banner-ad-container');
 
-console.log('URl: ' + url);
+    url = '//' + domain + '/?parentUrl=' + thisUrl;
 
-if (bannerAdEl) {
-    console.log('Deleting ad...');
-    bannerAdEl.parentNode.removeChild(bannerAdEl);
-}
+[
+    {
+        rx: /guardian|localhost/,
+        targetSelector: '.content__secondary-column .js-mpu-ad-slot',
+        position: 'beforebegin',
+        hideableSelector: '.top-banner-ad-container'
+    },
+    {
+        rx: /telegraph/,
+        targetSelector: '.oneThird',
+        position: 'afterbegin',
+        hideableSelector: null
+    }
+].forEach(function (spec) {
+    var targetEl,
+        hideableEl;
 
-if (articleEl) {
-    console.log('Inserting Free Press: ' + url);
-    articleEl.insertAdjacentHTML('beforebegin', '<iframe src="' + url + '" style="border: 0; float:  left; clear: left; width: 100%; height: 600px; margin: 0 0 72px 0;"></iframe>')
-}
+    if (thisUrl.match(spec.rx)) {
+        targetEl = document.querySelector(spec.targetSelector);
+        hideableEl = document.querySelector(spec.hideableSelector);
+
+        if (hideableEl) {
+            hideableEl.parentNode.removeChild(hideableEl);
+        }
+
+        if (targetEl) {
+            targetEl.insertAdjacentHTML(spec.position, '<iframe src="' + url + '" style="border: 0; float:left; clear:left; width:100%; height:600px; margin: 0 0 72px 0; background:#fff;"></iframe>')
+        }
+    }
+});
