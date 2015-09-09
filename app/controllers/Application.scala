369b4e38fa241b996cfed7b32361c7581344e902
Francis Carr <fran@fcarr.com>
2015-09-10 00:17:49 +0100

    Start associating real content only

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,6 +1,8 @@
 package controllers
 
 import graph.Database
+import model.{Basic, OpenGraphElement}
+import opengraph.FreePressOpenGraph
 import play.api.libs.json.{JsError, JsSuccess, Json}
 import play.api.mvc._
 
@@ -10,20 +12,26 @@ object Association {
   implicit val associationFormat = Json.format[Association]
 }
 
+//Carry Basic for now, remove to own type
+case class ValidAssociation(from: Basic, to: Basic, weight: Option[Long])
+
 object Application extends Controller {
 
   def index = Action { Ok(views.html.index(Database.allRelations) ) }
 
   def associateFormPost = Action { request =>
-    val maybeAssociation = for {
+    val maybeAssociation: Option[Association] = for {
       form <- request.body.asFormUrlEncoded
       from <- form.get("from").map(_.mkString)
       to <- form.get("to").map(_.mkString)
     } yield Association(from, to, None)
 
-    maybeAssociation match {
-      case Some(assoc) =>
-        Database.associate(assoc)
+    val maybeValidAssociation: Option[(Association, ValidAssociation)] =
+      maybeAssociation.flatMap(assoc => checkAssociation(assoc).map(assoc -> _))
+
+    maybeValidAssociation match {
+      case Some((assoc, validAssoc)) =>
+        Database.associate(assoc, validAssoc)
         SeeOther("/")
       case None =>
         InternalServerError("Incorrect form data")
@@ -33,8 +41,15 @@ object Application extends Controller {
   def associate = Action { request =>
     request.body.asJson.map(_.validate[Association]) match {
       case Some(JsSuccess(assoc, _)) =>
-        val result = Database.associate(assoc)
-        Ok(result.toString)
+        val maybeValidAssociation: Option[ValidAssociation] = checkAssociation(assoc)
+
+        maybeValidAssociation match {
+          case Some(validAssoc) =>
+            val result = Database.associate(assoc, validAssoc)
+            Ok(result.toString)
+          case None =>
+            InternalServerError("Not a valid association: could not parse OpenGraph data")}
+
       case Some(JsError(_)) => InternalServerError("Feed me the correct JSON")
       case None => InternalServerError("Feed me JSON")
     }
@@ -47,5 +62,9 @@ object Application extends Controller {
       case _ => InternalServerError
     }
   }
+
+  private def checkAssociation(assoc: Association): Option[ValidAssociation] =
+    FreePressOpenGraph.getTwo(assoc.from, assoc.to)
+      .map{ case (from: Basic, to: Basic) => ValidAssociation(from, to, assoc.weight)}
 }
 
--- app/graph/Database.scala
+++ app/graph/Database.scala
@@ -1,13 +1,13 @@
 package graph
 
-import controllers.Association
+import controllers.{ValidAssociation, Association}
 import org.anormcypher._
 
 import org.anormcypher.CypherParser._
 import play.api.libs.json.Json
 
 
-case class Article(id: String)
+case class Article(id: String, title: Option[String])
 case class Relations(article: Article, relations: List[Article])
 
 object Article {
@@ -21,17 +21,20 @@ object Relations {
 object Database {
   implicit val databaseConnection = Neo4jREST(username="neo4j", password="abc")
 
-  def associate(association: Association): Boolean = {
+  def associate(association: Association, validAssociation: ValidAssociation): Boolean = {
     val result = Cypher(
       """
-        |MERGE (from {id:{from}})
-        |MERGE (to {id:{to}})
+        |MERGE (from {id:{from}, title:{fromTitle}})
+        |MERGE (to {id:{to}, title:{toTitle}})
         |MERGE p=(from)-[:related]->(to)
         |RETURN p
       """.stripMargin)
     .on(
         "from" -> association.from,
-        "to" -> association.to)
+        "fromTitle" -> validAssociation.from.title,
+        "to" -> association.to,
+        "toTitle" -> validAssociation.to.title
+      )
     .apply()
 
     result.nonEmpty
@@ -47,20 +50,21 @@ object Database {
       .as(str("article.id") ~ str("relation.id") map(flatten) *)
 
     relations.headOption.map { f =>
-      Relations(Article(f._1), relations.map(r => Article(r._2)))
+      Relations(Article(f._1, None), relations.map(r => Article(r._2, None)))
     }
   }
 
   def allRelations: List[Relations] = {
-    val relations: List[(String, String)] = Cypher(
+    val relations = Cypher(
       """
         |MATCH (article)-[:related]-(relation)
-        |RETURN article.id, relation.id;
-      """.stripMargin)
-      .as(str("article.id") ~ str("relation.id") map(flatten) *)
+        |RETURN article.id, article.title, relation.id, relation.title;
+      """.stripMargin)().map { row =>
+      (row[String]("article.id"), row[Option[String]]("article.title"), row[String]("relation.id"), row[Option[String]]("relation.title"))
+    }
 
-    relations.map(_._1).distinct.map { id =>
-      Relations(Article(id), relations.filter(_._1 == id).map(r => Article(r._2)))
+    relations.toList.map(r => r._1 -> r._2).distinct.map { case (id, title) =>
+      Relations(Article(id, title), relations.toList.filter(_._1 == id).map{ case (_, _, i, t) => Article(i, t)})
     }
   }
 }
