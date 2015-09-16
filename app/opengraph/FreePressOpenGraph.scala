89df7ec97ee5f7ac43a4fa8fa08d1d8a4bd63f04
Francis Carr <fran@fcarr.com>
2015-09-17 00:19:47 +0100

    Add implicit global execution context for OpenGraph use

--- app/opengraph/FreePressOpenGraph.scala
+++ app/opengraph/FreePressOpenGraph.scala
@@ -4,6 +4,8 @@ import model.{OpenGraphElement, OpenGraphRequest}
 
 object FreePressOpenGraph {
 
+  import scala.concurrent.ExecutionContext.Implicits.global
+
   def getTwo(urlOne: String, urlTwo: String): Option[(OpenGraphElement, OpenGraphElement)] =
     for {
       openGraphElementOne <- getUrl(urlOne)
