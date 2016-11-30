79820a8fece0a4c0c41e84ac4d6d8ccfced67126
Stephan Fowler <stephanfowler@gmail.com>
2016-11-30 11:56:49 +0000

    Wording

--- public/build/client.js
+++ public/build/client.js
@@ -127,7 +127,7 @@ var Items = React.createClass({
             React.createElement(
                 "div",
                 { className: "instructions" },
-                "Drop related articles below. Upvote the best."
+                "Disagree? Drag & drop alternative articles below"
             ),
             React.createElement(
                 "div",
